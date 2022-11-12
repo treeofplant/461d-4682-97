d8a686ce36f1eed192e92c1da0c4d9d3ad75b925
hu <77543293+treeofplant@users.noreply.github.com>
2022-11-13 00:04:41 +0400

    Update actional.lua

--- actional.lua
+++ actional.lua
@@ -1,28 +1,28 @@
 if game.PlaceId == 855499080 then
-    suportedgayme = "yes"
+    game.CoreGui.loader123.Frame.keepthis.BackgroundTransparency = 1
     
     elseif game.PlaceId == 662417684 then
-    suportedgayme = "yes"
+    game.CoreGui.loader123.Frame.keepthis.BackgroundTransparency = 1
 
     elseif game.PlaceId == 4913581664 then
-    suportedgayme = "yes"
+    game.CoreGui.loader123.Frame.keepthis.BackgroundTransparency = 1
 
     elseif game.PlaceId == 1240123653 then
-    suportedgayme = "yes"
+    game.CoreGui.loader123.Frame.keepthis.BackgroundTransparency = 1
 
     elseif game.PlaceId == 1632210982 then
-    suportedgayme = "yes"
+    game.CoreGui.loader123.Frame.keepthis.BackgroundTransparency = 1
 
     elseif game.PlaceId == 9759729519 then
-    suportedgayme = "yes"
+    game.CoreGui.loader123.Frame.keepthis.BackgroundTransparency = 1
 
     elseif game.PlaceId == 10950394697 then
-    suportedgayme = "yes"
+    game.CoreGui.loader123.Frame.keepthis.BackgroundTransparency = 1
 
     elseif game.PlaceId == 1962086868 then
-    suportedgayme = "yes"
+    game.CoreGui.loader123.Frame.keepthis.BackgroundTransparency = 1
 
     elseif game.PlaceId == 3582763398  then
-    suportedgayme = "yes"
+    game.CoreGui.loader123.Frame.keepthis.BackgroundTransparency = 1
 
 end
