2d01d26c0c94fff0c63b9452832392bd6c8f9fee
hu <77543293+treeofplant@users.noreply.github.com>
2023-10-12 19:31:15 +0400

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,16 +1,1 @@
-pcall(function()
-    local mt = getrawmetatable(game)
-make_writeable(mt)
-
-local namecall = mt.__namecall
-
-mt.__namecall = newcclosure(function(self, ...)
-    local method = getnamecallmethod()
-    local args = {...}
-
-    if method == "deleteTimeout" then
-        return true
-    end
-    return namecall(self, ...)
-end)
-  end)
+print(" ")
