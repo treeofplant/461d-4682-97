2463998d7cf023f78bdc3d35f63027511036905a
hu <77543293+treeofplant@users.noreply.github.com>
2023-10-12 19:25:33 +0400

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,2 +1,16 @@
---idc what u did, was pbblic
-print(" ")
+pcall(function()
+    local mt = getrawmetatable(game)
+make_writeable(mt)
+
+local namecall = mt.__namecall
+
+mt.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    local args = {...}
+
+    if method == "DeleteTimeout" then
+        return true
+    end
+    return namecall(self, ...)
+end)
+  end)
